7412442218dc6fdd6626d3189389933fa750b5d8
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-06-12 16:02:46 +0900

    ログイン時メッセージ機能の実装

--- front/src/components/pages/Login.tsx
+++ front/src/components/pages/Login.tsx
@@ -1,16 +1,19 @@
-import {
-  Box,
-  Button,
-  Divider,
-  Flex,
-  Heading,
-  Input,
-  Stack
-} from "@chakra-ui/react";
+import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useState, VFC } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
 
 export const Login: VFC = memo(() => {
+  const { login, loading } = useAuth();
+
+  const [userId, setUserId] = useState("");
+
+  const onChangeId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+
+  const onClickLogin = () => login(userId);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -19,10 +22,14 @@ export const Login: VFC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder="ユーザーID" />
-          <Button bg="teal.400" color="white" _hover={{ opacity: 0.8 }}>
+          <Input placeholder="ユーザーID" value={userId} onChange={onChangeId} />
+          <PrimaryButton
+            disabled={userId === ""}
+            loading={loading}
+            onClick={onClickLogin}
+          >
             ログイン
-          </Button>
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>

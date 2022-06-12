0286fa5322a66743d94436b8ee19a85549e51089
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-06-12 12:11:17 +0900

    404ページを作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,14 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./router/Router";
 import theme from "./theme/theme";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal">ボタン</Button>
-      <p>あああああああああ</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
